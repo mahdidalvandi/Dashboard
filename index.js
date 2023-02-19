4a560869877edd4c4cd2cce287b41998dc78a66d
MahdiDalvandi <118760616+mahdidalvandi@users.noreply.github.com>
2023-02-20 00:14:51 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,75 @@
-import cors from "cors";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import express from "express";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
-import { expressCspHeader, INLINE, NONE, SELF } from "express-csp-header";
-import clientRoutes from "./routes/client.js";
-import generalRoutes from "./routes/general.js";
-import managementRoutes from "./routes/management.js";
-import salesRoutes from "./routes/sales.js";
-import User from "./models/User.js";
-import Product from "./models/Product.js";
-import ProductStat from "./models/ProductStat.js";
-import Transaction from "./models/Transaction.js";
-import OverallStat from "./models/OverallStat.js";
-import AffiliateStat from "./models/AffiliateStat.js";
-import path from "path";
-import {
-  dataUser,
-  dataProduct,
-  dataProductStat,
-  dataTransaction,
-  dataOverallStat,
-  dataAffiliateStat,
-} from "./data/index.js";
-
-/* CONFIGURATION */
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(morgan("common"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-helmet.contentSecurityPolicy({
-  useDefaults: false,
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "http://localhost:5001/"], // scripts from example.com are now trusted
-    objectSrc: ["'none'"],
-    upgradeInsecureRequests: [],
-  },
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const REACT_APP_BASE_URL = "http://localhost:5001/";
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+    "Admins",
+    "Performance",
+    "Dashboard",
+  ],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "/client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "Get",
+        params: {
+          page,
+          pageSize,
+          sort,
+          search,
+        },
+      }),
+      providesTags: ["Transactions"],
+    }),
+    getGeography: build.query({
+      query: () => "/client/geography",
+      providesTags: ["Geography"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "management/admins",
+      providesTags: ["Admins"],
+    }),
+    getUserPerformance: build.query({
+      query: (id) => `management/performance/${id}`,
+      providesTags: ["Performance"],
+    }),
+    getDashboard: build.query({
+      query: (id) => `general/dashboard`,
+      providesTags: ["Dashboard"],
+    }),
+  }),
 });
-/* ROUTES */
-app.use("/client", clientRoutes);
-app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes);
-
-/* MONGOOSE SETUP */
-
-const PORT = process.env.PORT || 9000;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Mongo Connected");
-  })
-  .catch((error) => console.log(`${error} did not connect`));
-app.listen(PORT, () =>
-  // Product.insertMany(dataProduct),
-  // ProductStat.insertMany(dataProductStat)
-  // User.insertMany(dataUser);
-  // Transaction.insertMany(dataTransaction)
-  // OverallStat.insertMany(dataOverallStat)
-  // AffiliateStat.insertMany(dataAffiliateStat)
-  console.log(`Server Port: ${PORT}`)
-);
-
-const dirname = path.resolve();
-app.use(express.static(path.join(dirname, "./client/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(dirname, "./client/build/index.html"))
-);
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = api;
